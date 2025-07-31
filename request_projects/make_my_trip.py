368f0dd905489ecb6224f98592af3467e830451c
Madri <madrichauhan@gmail.com>
2025-07-31 12:14:54 +0530

    Auto push on 2025-07-31 12:13:59

--- request_projects/make_my_trip.py
+++
@@ -1,9 +0,0 @@
-url='https://www.makemytrip.com/hotels/hotel-listing/?checkin=06192025&city=CTGOI&checkout=06202025&roomStayQualifier=2e0e&locusId=CTGOI&country=IN&locusType=city&searchText=StayVista+at+Prairie+Haven%2C+Goa&regionNearByExp=3&rsc=1e2e0e&topHtlId=202312251117533610'
-urq='https://www.makemytrip.com/hotels/hotel-listing/?checkin=06192025&checkout=06202025&city=CTGOI&country=IN&locusId=CTGOI&locusType=city&regionNearByExp=3&roomStayQualifier=2e0e&rsc=1e2e0e&searchText=Casa%20Sunshine%20Goa%2C%20Goa&topHtlId=202309221343178740'
-
-stay_vista_hotels=['Casa Sunshine','Prairie Haven','Casa Del Lusso','Villa Sunbird','Brew & Breeze',
-                   'Casa San Antonio','Flint @ Casa San Antonio','Ruby @ Casa San Antonio','Casa Vida',
-                   'Casa Vida Oro','Casa Vida Zen','Tia @ Casa Vida','Leo @ Casa Vida','Casa Moringa',
-                   'A Portuguese Tale','Palm @ A Portuguese Tale','Coco @ A Portuguese Tale','Waterlily']
-
-mmt_url='https://www.makemytrip.com/hotels/hotel-listing/?checkin=2025-07-01&checkout=2025-07-02&roomStayQualifier=2e0e&city=GOA&searchText=Casa%20Sol'
